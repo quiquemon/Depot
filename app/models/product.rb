06067d1b7cecaa5a32d5ec08b49a2138a199fc85
PROGRAMACION2 <PROGRAMACION2@LAPTOP-PT7LJ4TU>
2016-12-19 12:01:20 -0600

    Terminadas validaciones sobre el modelo 'Product'

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+	validates :title, uniqueness: true
+	validates :title, length: {
+		in: 10..255,
+		too_short: "%{count} es el número mínimo de caracteres requeridos para el título.",
+		too_long: "%{count} es el número máximo de caracteres permitidos para el título."
+	}
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
 end
