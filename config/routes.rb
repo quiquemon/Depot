942054d99fd979ac5539d2cdc5c30952c781b35c
PROGRAMACION2 <PROGRAMACION2@LAPTOP-PT7LJ4TU>
2016-12-19 15:49:04 -0600

    Controlador y vistas de 'Store' creadas junto con estilos.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get 'store/index'
+	resources :products
+	
+	# You can have the root of your site routed with 'root'
+	# Just remember to delete public/index.html
+	root to: 'store#index', as: 'store'
 end
