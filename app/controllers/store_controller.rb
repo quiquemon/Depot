b9e2385d869e3512842a5175c9088b728a0c4494
PROGRAMACION2 <PROGRAMACION2@LAPTOP-PT7LJ4TU>
2016-12-20 13:07:57 -0600

    Carrito de compra inicial (tablas Carts y LineItems creadas).

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
 	def index
 		@products = Product.order(:title)
+		
+		if session[:count].nil?
+			session[:count] = 0
+		else
+			session[:count] += 1
+		end
+		
+		@access_count = session[:count]
 	end
 end
